bfb5ffc16caabdbe40ebe607c4ef1d1c16a36118
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2023-12-21 16:32:09 -0800

    toggle function

--- src/app/quiz-home/quiz-home.component.ts
+++ src/app/quiz-home/quiz-home.component.ts
@@ -1,12 +1,20 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-quiz-home',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './quiz-home.component.html',
   styleUrl: './quiz-home.component.scss'
 })
 export class QuizHomeComponent {
+
+  isDarkMode = false
+
+  toggleTheme(){
+   this.isDarkMode= !this.isDarkMode
+   console.log('hello')
+  }
 
 }
